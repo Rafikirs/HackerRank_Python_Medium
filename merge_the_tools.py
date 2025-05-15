686559ef83cf3770e75ee38286ddf01da8b0ba00
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 21:07:33 +0200

    Update merge_the_tools.py

--- merge_the_tools.py
+++ merge_the_tools.py
@@ -1,3 +1,7 @@
+# Exercise: Merge the tools
+# URL: https://www.hackerrank.com/challenges/merge-the-tools/problem?isFullScreen=true
+# Description: Print substrings of a string while removing duplicated letters within each substring
+
 def merge_the_tools(string, k):
     for i in range(0, len(string), k):
         substring = string[i:i+k]
