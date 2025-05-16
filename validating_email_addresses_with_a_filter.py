036c4aa4ecd90f5b30cb7582a2e8f7ee8c61af37
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:15:50 +0200

    Update and rename validating_email_address.py to validating_email_addresses_with_a_filter.py

--- validating_email_addresses_with_a_filter.py
+++ validating_email_addresses_with_a_filter.py
@@ -1,4 +1,4 @@
-# Exercise: Validating Email Address
+# Exercise: Validating Email Addresses With A Filter
 # URL: https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem?isFullScreen=true
 # Description: Validating an email address based on an expected format and specific digits
 
