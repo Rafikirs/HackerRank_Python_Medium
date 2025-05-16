73755be7226fe6f22b6d5f814b4788c8c280904e
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:22:22 +0200

    Update reduce_function.py

--- reduce_function.py
+++ reduce_function.py
@@ -1,3 +1,8 @@
+# Exercise: Reduce Function
+# URL: https://www.hackerrank.com/challenges/reduce-function/problem?isFullScreen=true
+# Description: Print only one line containing the numerator and denominator of the product 
+# of the numbers in the list in its simplest form
+
 from fractions import Fraction
 from functools import reduce
 
