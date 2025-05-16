c6a753d8e955a3c64a114d023e795bcbae3eefff
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:15:05 +0200

    Update validating_email_address.py

--- validating_email_address.py
+++ validating_email_address.py
@@ -1,3 +1,7 @@
+# Exercise: Validating Email Address
+# URL: https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem?isFullScreen=true
+# Description: Validating an email address based on an expected format and specific digits
+
 def fun(s):
     pattern = r'^[\w-]+@[A-Za-z0-9]+\.[A-Za-z]{1,3}$'
     return re.match(pattern, s) is not None
