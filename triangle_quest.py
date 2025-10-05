531d8c045620ec754079d586aedeac485703653e
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-05 21:52:43 +0200

    Create triangle_quest.py

---
+++ triangle_quest.py
@@ -0,0 +1,6 @@
+# Exercise: Triangle Quest
+# URL: https://www.hackerrank.com/challenges/python-quest-1/problem?isFullScreen=true
+# Description: Creating a specific triangle of numbers in only 2 lines
+
+for i in range(1,int(input())): 
+    print(i * (10**i // 9))
