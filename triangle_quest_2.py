5e8a9be3e330ca884cccf06c77d0b81416751f44
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-05 20:49:15 +0200

    Create triangle_quest_2.py

---
+++ triangle_quest_2.py
@@ -0,0 +1,6 @@
+# Exercise: Triangle Quest 2
+# URL: https://www.hackerrank.com/challenges/triangle-quest-2/problem?isFullScreen=true
+# Description: Create a palindromic triangle on only 2 lines
+
+for i in range(1,int(input())+1):
+    print((10**i // 9)**2)
