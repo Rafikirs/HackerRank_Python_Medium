50eac6b0606ec3c44b40b83704ddb84e56b65468
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-11 16:40:54 +0200

    Create words_score.py

---
+++ words_score.py
@@ -0,0 +1,24 @@
+# Exercise: Words Score
+# URL: https://www.hackerrank.com/challenges/words-score/problem?isFullScreen=true
+# Description: Debugging a function which output the score of a list of strings depending on the number of vowels
+
+def is_vowel(letter):
+    return letter in ['a', 'e', 'i', 'o', 'u', 'y']
+
+def score_words(words):
+    score = 0
+    for word in words:
+        num_vowels = 0
+        for letter in word:
+            if is_vowel(letter):
+                num_vowels += 1
+        if num_vowels % 2 == 0:
+            score += 2
+        else:
+            score += 1
+    return score
+
+
+n = int(input())
+words = input().split()
+print(score_words(words))
